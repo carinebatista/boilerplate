ac9b5599b910e101fa388767ca67a226d57a352c
Carine Batista <carinedcb@hotmail.com>
2021-06-20 23:09:56 -0300

    first Commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '../../styles/Home.module.css'
 
-export default function Home() {
+type Props ={
+  title: string;
+}
+
+export default function Home({title = "React"} : Props) {
   return (
     <div className={styles.container}>
       <Head>
@@ -13,11 +17,11 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">{title}</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by ting{' '}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
